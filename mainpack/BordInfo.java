29299652aa04b6a57adec92a4a7d6def89afc20b
himihiromu <himihiromu@icloud.com>
2019-04-25 00:04:06 +0900

    hensu-kaitei

--- mainpack/BordInfo.java
+++ mainpack/BordInfo.java
@@ -21,25 +21,52 @@ class BordInfo {
 	 * オセロの盤面に壁を設定し、初期化するコンストラクタ
 	 */
 	BordInfo() {
-		for(int v = 0; v < 10; v++) {
-			int s = 0;
-			if((v == 0) || (v == 9)) {
-				while(true) {
-					if(s > 10) {
-						break;
-					}
-					bord[v][s] = -1;
-					s++;
+		/* 端に-1を設定するループ処理 */
+		for(int vertical = 0; vertical < 10; vertical++) {
+
+			int lateral = 0;
+
+			/* 行の両端の添え字になっていれば-1を設定 */
+			if((vertical == 0) || (vertical == 9)) {
+
+				while(lateral < 10) {
+
+					bord[vertical][lateral] = -1;
+					lateral++;
 				}
+				/* 行の両端ではないので壁と空白を設定 */
 			} else {
-				bord[v][s] = -1;
-				s++;
-				while(s < 10) {
-					bord[v][s] = 0;
-					s++;
+
+				/* 壁の-1を設定 */
+				bord[vertical][lateral] = -1;
+				lateral++;
+
+				/* 壁ではないので空白を設定 */
+				while(lateral < 10) {
+
+					bord[vertical][lateral] = 0;
+					lateral++;
+
 				}
-				bord[v][s] = -1;
+				/* 壁の-1を設定 */
+				bord[vertical][lateral] = -1;
 			}
+		}
+	}
+
+	/**
+	 * フラグを引数にその手番の石の色を返すメソッド
+	 *
+	 * @param firstFlag
+	 *            先攻か後攻かのフラグ
+	 * @return 石の色
+	 */
+	int stoneColor(boolean firstFlag) {
+		/* 先攻は黒、後攻は白 */
+		if(firstFlag) {
+			return 2;
+		} else {
+			return 1;
 		}
 	}
 
@@ -48,10 +75,14 @@ class BordInfo {
 	 *
 	 */
 	public void output() {
-		for(int v = 0; v < 10; v++) {
-			for(int s = 0; s < 10; s++) {
-				System.out.print(bc.intToStone(bord[v][s]));
+		/* 縦のループ */
+		for(int vertical = 0; vertical < 10; vertical++) {
+			/* 横のループ */
+			for(int lateral = 0; lateral < 10; lateral++) {
+				/* 盤面の出力 */
+				System.out.print(bc.intToStone(bord[vertical][lateral]));
 			}
+			/* 改行 */
 			System.out.println();
 		}
 
@@ -61,35 +92,44 @@ class BordInfo {
 	 * 指定した座標の情報を返すメソッド
 	 * 引数で指定した座標の位置の数字を返す
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param vertical
 	 *            縦の座標
 	 * @return -1~3の座標
 	 */
-	public int partOut(int s, int v) {
-		return bord[v][s];
+	public int partOut(int lateral, int vertical) {
+		/* 指定された座標の状態を返り値に設定 */
+		return bord[vertical][lateral];
 	}
 
 	/**
 	 * 指定した座標の石を裏返すメソッド
 	 * 引数で指定した座標の位置の石を反転させる
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param vertical
 	 *            縦の座標
 	 * @throws ReturnErrException
 	 *             石が無いときに投げられる例外
 	 */
-	public void stoneReturn(int s, int v) throws ReturnErrException {
-		int work = bord[v][s];
+	public void stoneReturn(int lateral, int v) throws ReturnErrException {
+
+		int work = bord[v][lateral];		//見やすいようにいったん退避
+		/* 格納された数字によって処理を変える */
 		switch (work) {
+
+		/* 白の場合 */
 		case 1:
-			bord[v][s] = 2;
+			bord[v][lateral] = 2;			//黒を代入
+
+			/* 黒の場合 */
 		case 2:
-			bord[v][s] = 1;
+			bord[v][lateral] = 1;			//白を代入
+
 		default:
+			/* 壁、空白、範囲外の座標などの場合 */
 			throw new ReturnErrException("裏返す石が無いです");
 		}
 	}
@@ -97,22 +137,23 @@ class BordInfo {
 	/**
 	 * 指定した座標に石を置くメソッド
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param vertical
 	 *            縦の座標
-	 * @param turn
+	 * @param firstFlag
 	 *            先攻か後攻かのフラグ
 	 * @throws PutErrException
 	 */
-	public void stonePut(int s, int v, boolean turn) throws PutErrException {
-		if(!(bord[v][s] == 0)) {
+	public void stonePut(int lateral, int vertical, boolean firstFlag) throws PutErrException {
+		/**/
+		if(!(bord[vertical][lateral] == 0)) {
 			throw new PutErrException("もう既にこの場所には石が置かれています");
 		}
-		if(turn) {
-
+		if(firstFlag) {
+			bord[vertical][lateral] = 2;
 		} else {
-
+			bord[vertical][lateral] = 1;
 		}
 	}
 }
--- mainpack/BordReturn.java
+++ mainpack/BordReturn.java
@@ -1,6 +1,7 @@
 package mainpack;
 
 import ExceptionSummary.PutErrException;
+import ExceptionSummary.ReturnErrException;
 
 /**
  *
@@ -9,6 +10,8 @@ import ExceptionSummary.PutErrException;
  */
 public class BordReturn {
 	BordInfo bi = null;
+	boolean[] returnJudgment = new boolean[9];
+	DecideDirection DD = new DecideDirection();
 
 	public BordReturn(BordInfo bi) {
 		this.bi = bi;
@@ -18,94 +21,115 @@ public class BordReturn {
 	 * 石を盤面に置くメソッド
 	 * 座標を引数にして盤面に石を置き、同じ色で挟んだ石を裏返す。
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param Vertical
 	 *            縦の座標
+	 * @throws PutErrException
+	 * @throws ReturnErrException
 	 */
-	void returnStone(int s, int v) {
+	void returnStone(int lateral, int vertical, boolean firstFlag) throws PutErrException, ReturnErrException {
+		for(int i = 0; i < 10; i++) {
+			returnJudgment[i] = false;
+		}
 
+		int stone = bi.stoneColor(firstFlag);
+		int ldir;
+		int vdir;
+
+		if(returnSerch(lateral, vertical, firstFlag, true)) {
+			for(int direction = 1; direction < 10; direction++) {
+
+				if(returnJudgment[direction - 1]) {
+					ldir = DD.sideDirection(direction);
+					vdir = DD.verticalDirection(direction);
+
+					bi.stonePut(lateral, vertical, firstFlag);
+					lateral += ldir;
+					vertical += vdir;
+
+					while(bi.stoneColor(firstFlag) != stone) {
+						bi.stoneReturn(lateral, vertical);
+						lateral += ldir;
+						vertical += vdir;
+						stone = bi.partOut(lateral, vertical);
+					}
+				}
+			}
+		}
 	}
 
 	/**
 	 * 石を裏返せるかを確認するメソッド
 	 * 座標を引数にしてその座標に石を置けるかを確認する。
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param vertical
 	 *            縦の座標
+	 * @param firstFlag
+	 *            先攻か後攻かを受け取る
+	 * @param returnFlag
+	 *            裏返し処理をするかしないか。
 	 * @return 入れ替えられたかどうか
 	 * @throws PutErrException
 	 */
-	boolean returnSerch(int s, int v, boolean firstFlag) throws PutErrException {
-		if(this.bi.partOut(s, v) == -1) {
+	boolean returnSerch(int lateral, int vertical, boolean firstFlag, boolean returnFlag) throws PutErrException {
+		boolean canDoFlag = false;
+		if(this.bi.partOut(lateral, vertical) == -1) {
 			throw new PutErrException("ここは壁です");
 		}
-		int sdir, vdir;
+		int ldir, vdir;
 
 		for(int direction = 1; direction < 10; direction++) {
-			sdir = direction % 3;
-			vdir = direction / 3;
-			switch (sdir) {
-			case 0:
-				sdir = 1;
-				break;
-			case 1:
-				sdir = -1;
-				break;
-			case 2:
-				sdir = 0;
-				break;
-			}
-			switch (vdir) {
-			case 0:
-				vdir = -1;
-				break;
-			case 1:
-				vdir = 0;
-				break;
-			case 2:
-				vdir = 1;
-				break;
-			}
-			if((sdir == 0) && (vdir == 0)) {
+			ldir = DD.sideDirection(direction);
+			vdir = DD.verticalDirection(direction);
+
+			if((ldir == 0) && (vdir == 0)) {
 				continue;
 			}
-			if(returnSerchOneDirection(s, v, sdir, vdir, firstFlag)) {
-				return true;
+			if(returnSerchOneDirection(lateral, vertical, ldir, vdir, firstFlag)) {
+				if(returnFlag) {
+					returnJudgment[direction - 1] = true;
+					canDoFlag = true;
+				} else {
+					return true;
+				}
 			}
 		}
-		return false;
+		return canDoFlag;
+	}
 
+	boolean returnSerch(int s, int vertical, boolean firstFlag) throws PutErrException {
+		return returnSerch(s, vertical, firstFlag, false);
 	}
 
 	/**
 	 * 指定した座標から指定した方向に石を裏返すことができるかを調べるメソッド
 	 * 座標と方向を引数で指定し、その方向に石があればtrueを返す
 	 *
-	 * @param s
+	 * @param lateral
 	 *            横の座標
-	 * @param v
+	 * @param vertical
 	 *            縦の座標
-	 * @param sdir
+	 * @param ldir
 	 *            横の方向
 	 * @param vdir
 	 *            縦の方向
 	 * @return 裏返すことができればtrue、できなければfalse
 	 */
-	boolean returnSerchOneDirection(int s, int v, int sdir, int vdir, boolean firstFlag) {
+	boolean returnSerchOneDirection(int lateral, int vertical, int ldir, int vdir, boolean firstFlag) {
 		boolean endFlag = false;
-		int stone = this.bi.partOut(s, v);
+		int stone = this.bi.partOut(lateral, vertical);
 		int changeable = 0;
 
 		if(stone != 0) {
 			return false;
 		}
 
-		s += sdir;
-		v += vdir;
-		stone = this.bi.partOut(s, v);
+		lateral += ldir;
+		vertical += vdir;
+		stone = this.bi.partOut(lateral, vertical);
 
 		if(firstFlag) {
 			while((stone != -1) && (endFlag == false)) {
@@ -118,9 +142,9 @@ public class BordReturn {
 					endFlag = true;
 					break;
 				case 1:
-					s += sdir;
-					v += vdir;
-					stone = this.bi.partOut(s, v);
+					lateral += ldir;
+					vertical += vdir;
+					stone = this.bi.partOut(lateral, vertical);
 					changeable++;
 					break;
 				}
@@ -136,9 +160,9 @@ public class BordReturn {
 					endFlag = true;
 					break;
 				case 2:
-					s += sdir;
-					v += vdir;
-					stone = this.bi.partOut(s, v);
+					lateral += ldir;
+					vertical += vdir;
+					stone = this.bi.partOut(lateral, vertical);
 					changeable++;
 					break;
 				}
@@ -149,5 +173,42 @@ public class BordReturn {
 		} else {
 			return true;
 		}
+	}
+}
+
+class DecideDirection {
+	int sideDirection(int direction) {
+		int ldir;
+		ldir = direction % 3;
+		switch (ldir) {
+		case 0:
+			ldir = 1;
+			break;
+		case 1:
+			ldir = -1;
+			break;
+		case 2:
+			ldir = 0;
+			break;
+		}
+		return ldir;
+	}
+
+	int verticalDirection(int direction) {
+		int vdir;
+		vdir = direction / 3;
+		switch (vdir) {
+		case 0:
+			vdir = -1;
+			break;
+		case 1:
+			vdir = 0;
+			break;
+		case 2:
+			vdir = 1;
+			break;
+		}
+		return vdir;
+
 	}
 }
